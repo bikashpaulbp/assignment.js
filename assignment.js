093f33c521c8fa9060a7b59fa2f1fd367303d237
bikashpaulbp <bikashpaulbp92@gmail.com>
2021-01-22 15:54:14 +0600

    done

--- assignment.js
+++ assignment.js
@@ -1,1 +1,69 @@
-// 
+// https://github.com/bikashpaulbp/assignment.js
+
+function kilometerToMeter(kilo){
+    var result = 1000 * kilo;
+    return result;
+}
+
+
+function budgetCalculator(watch, mobile, laptop){
+   var watchPrice = watch * 50;
+   var mobilePrice = mobile * 100;
+   var laptopPrice = laptop * 500;
+   var totalBudget = watchPrice + mobilePrice + laptopPrice;
+    return totalBudget;
+    
+}
+
+
+function hotelCost(day){
+    
+    var totalCost = 0;
+
+    for(var i=1; i<=day; i++){
+
+
+        if(i <= 10){
+            totalCost = totalCost +  100;
+        }
+        
+        else if(i >= 11 && i <= 20 ){
+
+            totalCost = totalCost +  80;
+        }
+
+       else {
+
+             totalCost = totalCost +  50;
+        }
+
+        
+    }
+
+    
+    return totalCost;
+
+}
+
+
+
+function megaFriend(friendList){
+
+    
+
+    if(friendList.length == 0){
+        return "Empty List";
+    }
+
+    var maxName = friendList[0];
+
+    for(var i=1; i<friendList.length; i++){
+
+        if(maxName.length < friendList[i].length){
+            maxName = friendList[i];
+        }
+    }
+    
+    return maxName;
+}
+
